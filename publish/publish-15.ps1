22da9981920fe2e61e563ab528f41f837e21f67d
Chaoses-Ib <Chaos-es@outlook.com>
2026-07-05 06:50:45 +0800

    feat: bump v1.5 version

--- publish/publish-15.ps1
+++ publish/publish-15.ps1
@@ -1,5 +1,5 @@
 ﻿# https://www.voidtools.com/forum/viewtopic.php?t=9787
-$version = "1.5.0.1413b"
+$version = "1.5.0.1416b"
 # https://www.voidtools.com/forum/viewtopic.php?f=12&t=9799
 $httpVersion = "1.0.4.5"
 $etpVersion = "1.0.2.5"
